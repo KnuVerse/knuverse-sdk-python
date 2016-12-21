4d00ab271aa7929407b8353daaa2a109b929800f
Ketan Murthy <kmurthy@knuverse.com>
2016-12-21 15:37:45 -0800

    Trying to fix the 2.7.9 compatibility error

--- knuverse/knufactor.py
+++ knuverse/knufactor.py
@@ -513,7 +513,7 @@ class Knufactor:
         }
 
         if mode:
-            print('WARNING: The "mode" parameter for enrollment_start is DEPRECATED and will be ignored.  To avoid '
+            print('WARNING: The "mode" parameter for enrollment_start is DEPRECATED and will be ignored.  To avoid '\
                   'incompatibility with a future release please stop providing it.', file=sys.stderr)
         if pin:
             data["pin"] = pin
