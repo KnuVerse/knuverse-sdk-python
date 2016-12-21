995de498693576bf3bd17fc83f761b18bc142333
Ketan Murthy <kmurthy@knuverse.com>
2016-12-21 15:55:10 -0800

    trying to enforce my will

--- knuverse/knufactor.py
+++ knuverse/knufactor.py
@@ -513,7 +513,8 @@ class Knufactor:
         }
 
         if mode:
-            print('WARNING: The "mode" parameter for enrollment_start is DEPRECATED and will be ignored.  To avoid incompatibility with a future release please stop providing it.', file=sys.stderr)
+            print('WARNING: The "mode" parameter for enrollment_start is DEPRECATED and will be ignored.  '
+                  'To avoid incompatibility with a future release please stop providing it.', file=sys.stderr)
         if pin:
             data["pin"] = pin
         if phone_number:
